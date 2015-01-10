9bb4494b235e7de45a1ea3923c9fa6a66cf73de1
morrain <626100676@qq.com>
2015-01-10 21:31:20 +0800

    完善slider组件

--- widget/slider/MoUI-slider.js
+++ widget/slider/MoUI-slider.js
@@ -9,8 +9,8 @@ author:morrain
     "use strict";
 
     var defaultOpitons = {
-        speed: 500, // 动画切换的速度
-        interval: 3000, // 动画切换的时间间隔
+        speed: 500, // 动画切换的速度 单位毫秒
+        interval: 2, // 动画切换的时间间隔 单位秒
         pause: true, // 鼠标移入进是否暂停
         loop: true, // 是否循环，最后一张再下一个会循环到第一张
         keys: false, // 是否支持键盘播放
@@ -45,7 +45,7 @@ author:morrain
 
             //如果不存在就创建组件实例
             if (!instance)
-                me.data('MoUI-slider', instance = new MoUISlider(me, option).init(me, option));
+                me.data('MoUI-slider', instance = new MoUISlider(me, option));
 
             if (typeof option === 'string') {
                 //当为方法调用时 调用组件方法
@@ -63,223 +63,209 @@ author:morrain
     };
 
     MoUISlider.prototype.constructor = MoUISlider;
+    /**
+     * [MoUISlider description]组件构造函数
+     * @param {[type]} ele    [description]对应的DOM元素
+     * @param {[type]} option [description]相应的配置对象
+     */
+    function MoUISlider(ele, option) {
+        var _ = this,
+            ele = $(ele);
 
-    function MoUISlider() {
-        //  Object clone
+        _.ele = ele;
+        _.o = $.extend(defaultOpitons, {
+            speed: ele.data('speed'),
+            interval: ele.data('interval'),
+            pause: ele.data('pause'),
+            loop: ele.data('loop'),
+            keys: ele.data('keys'),
+            dots: ele.data('dots'),
+            arrows: ele.data('arrows'),
+            prev: ele.data('prev'),
+            next: ele.data('next'),
+            fluid: ele.data('fluid'),
+            easing: ele.data('easing'),
+            autoplay: ele.data('autoplay')
+        }, ele.data("option"), option);
+
+        _.max = [ele.outerWidth(), ele.outerHeight()];
+        _.ul = ele.find('>ul');
+        _.li = _.ul.find('>li').each(function () {
+            var me = $(this),
+                width = me.outerWidth(),
+                height = me.outerHeight();
+
+            //  找到所有li中最大的宽高
+            if (width > _.max[0]) _.max[0] = width;
+            if (height > _.max[1]) _.max[1] = height;
+        });
+
+        _.i = 0; //当前slider的索引
+
+        ele.css({
+            'width': _.max[0],
+            'height': _.li.first().outerHeight(),
+            'overflow': 'hidden'
+        });
+
+        _.ul.css({
+            'position': 'relative',
+            'left': 0,
+            'width': (_.li.length * 100) + '%'
+        });
+
+        if (_.li.length === 0) return;
+        _.li.css({
+            'float': 'left',
+            'width': _.o.fluid ? ((100 / _.li.length) + '%') : (_.max[0] + 'px')
+        });
+
+        //是否为自动播放
+        _.o.autoplay && setTimeout(function () {
+
+            _.play();
+
+            //鼠标移入时暂停播放，移出时继续播放
+            if (_.o.pause) {
+                ele.bind({
+                    'mousemove': function () {
+                        _.stop()
+                    },
+                    'mouseout': function () {
+                        _.play();
+                    }
+                });
+            };
+        }, 0);
+
+        //添加键盘响应
+        _.o.keys && $(document).keydown(function (e) {
+
+            if (e.which == 37)
+                _.prev(); // 向左
+            else if (e.which == 39)
+                _.next(); // 向右
+            else if (e.which == 27)
+                _.stop(); // Esc
+
+        });
+
+        //添加点和箭头导航
+        _.o.dots && _._nav('dot');
+        _.o.arrows && _._nav('arrow');
+
+        //图片自适应调整
+        _.o.fluid && $(window).resize(function () {
+            _.r && clearTimeout(_.r);
+
+            _.r = setTimeout(function () {
+                var style = {
+                        height: _.li.eq(_.i).outerHeight()
+                    },
+                    width = _.ele.outerWidth();
+
+                _.ul.css(style);
+                style['width'] = Math.min(Math.round((width / _.ele.parent().width()) * 100), 100) + '%';
+                _.ele.css(style);
+                _.li.css({
+                    width: width + 'px'
+                });
+            }, 50);
+        }).resize();
+
+
+        return _;
+    }
+
+
+    MoUISlider.prototype.to = function (index) {
+
         var _ = this;
 
-        //  Set some options
-        _.o = {
-            speed: 500, // animation speed, false for no transition (integer or boolean)
-            interval: 3000, // interval between slides, false for no autoplay (integer or boolean)
-            pause: true, // pause on hover (boolean)
-            loop: true, // infinitely looping (boolean)
-            keys: false, // keyboard shortcuts (boolean)
-            dots: false, // display dots pagination (boolean)
-            arrows: false, // display prev/next arrows (boolean)
-            prev: '&larr;', // text or html inside prev button (string)
-            next: '&rarr;', // same as for prev option
-            fluid: false, // is it a percentage width? (boolean)
-            onBefore: false, // invoke before animation (function with argument)
-            onAfter: false, // invoke after animation (function with argument)
-            easing: 'swing', // easing function to use for animation
-            autoplay: true // enable autoplay on initialisation
-        };
-
-        _.init = function (el, o) {
-            //  Check whether we're passing any options in to MoUISlider
-            _.o = $.extend(_.o, o);
-
-            _.el = el;
-            _.ul = el.find('>ul');
-            _.max = [el.outerWidth() | 0, el.outerHeight() | 0];
-            _.li = _.ul.find('>li').each(function (index) {
-                var me = $(this),
-                    width = me.outerWidth(),
-                    height = me.outerHeight();
-
-                //  Set the max values
-                if (width > _.max[0]) _.max[0] = width;
-                if (height > _.max[1]) _.max[1] = height;
-            });
+        var o = _.o,
+            el = _.ele,
+            ul = _.ul,
+            li = _.li;
 
 
-            //  Cached vars
-            var o = _.o,
-                ul = _.ul,
-                li = _.li,
-                len = li.length;
+        $.isFunction(o.onBefore) && o.onBefore.call(_, el, li.eq(_.i));
 
-            //  Current indeed
-            _.i = 0;
+        //第一张
+        if (index < 0 && !o.loop) return;
+        else if (index < 0) index = li.length - 1;
 
-            //  Set the main element
-            el.css({
-                width: _.max[0],
-                height: li.first().outerHeight(),
-                overflow: 'hidden'
-            });
+        //最后一张
+        if (index >= li.length && !o.loop) return;
+        else if (index >= li.length) index = 0;
 
-            //  Set the relative widths
-            ul.css({
-                position: 'relative',
-                left: 0,
-                width: (len * 100) + '%'
-            });
-            if (o.fluid) {
-                li.css({
-                    'float': 'left',
-                    width: (100 / len) + '%'
-                });
-            } else {
-                li.css({
-                    'float': 'left',
-                    width: (_.max[0]) + 'px'
-                });
-            }
+        if (!ul.queue('fx').length) {
 
-            //  Autoslide
-            o.autoplay && setTimeout(function () {
-                if (o.interval | 0) {
-                    _.play();
+            var height = li.eq(index).outerHeight();
 
-                    if (o.pause) {
-                        el.on('mouseover mouseout', function (e) {
-                            _.stop();
-                            e.type == 'mouseout' && _.play();
-                        });
-                    };
-                };
-            }, 0);
+            el.animate({
+                'height': height
+            }, o.speed, o.easing);
 
-            //  Keypresses
-            if (o.keys) {
-                $(document).keydown(function (e) {
-                    var key = e.which;
-
-                    if (key == 37)
-                        _.prev(); // Left
-                    else if (key == 39)
-                        _.next(); // Right
-                    else if (key == 27)
-                        _.stop(); // Esc
-                });
-            };
-
-            //  Dot pagination
-            o.dots && nav('dot');
-
-            //  Arrows support
-            o.arrows && nav('arrow');
-
-            //  Patch for fluid-width sliders. Screw those guys.
-            if (o.fluid) {
-                $(window).resize(function () {
-                    _.r && clearTimeout(_.r);
-
-                    _.r = setTimeout(function () {
-                        var styl = {
-                                height: li.eq(_.i).outerHeight()
-                            },
-                            width = el.outerWidth();
-
-                        ul.css(styl);
-                        styl['width'] = Math.min(Math.round((width / el.parent().width()) * 100), 100) + '%';
-                        el.css(styl);
-                        li.css({
-                            width: width + 'px'
-                        });
-                    }, 50);
-                }).resize();
-            };
-
-            return _;
-        };
-
-        //  Move MoUISlider to a slide index
-        _.to = function (index, callback) {
-            if (_.t) {
-                _.stop();
-                _.play();
-            }
-            var o = _.o,
-                el = _.el,
-                ul = _.ul,
-                li = _.li,
-                current = _.i,
-                target = li.eq(index);
-
-            $.isFunction(o.onBefore) && !callback && o.onBefore(el, li.eq(current));
-
-            //  To slide or not to slide
-            if ((!target.length || index < 0) && o.loop == false) return;
-
-            //  Check if it's out of bounds
-            if (!target.length) index = 0;
-            if (index < 0) index = li.length - 1;
-            target = li.eq(index);
-
-            var speed = callback ? 5 : o.speed | 0,
-                easing = o.easing,
-                obj = {
-                    height: target.outerHeight()
-                };
-
-            if (!ul.queue('fx').length) {
-                //  Handle those pesky dots
-                el.find('.dot').eq(index).addClass('active').siblings().removeClass('active');
-
-                el.animate(obj, speed, easing) && ul.animate($.extend({
-                    left: '-' + index + '00%'
-                }, obj), speed, easing, function (data) {
-                    _.i = index;
-
-                    $.isFunction(o.onAfter) && !callback && o.onAfter(el, target);
-                });
-            };
-        };
-
-        //  Autoplay functionality
-        _.play = function () {
-            _.t = setInterval(function () {
-                _.to(_.i + 1);
-            }, _.o.interval | 0);
-        };
-
-        //  Stop autoplay
-        _.stop = function () {
-            _.t = clearInterval(_.t);
-            return _;
-        };
-
-        //  Move to previous/next slide
-        _.next = function () {
-            return _.stop().to(_.i + 1);
-        };
-
-        _.prev = function () {
-            return _.stop().to(_.i - 1);
-        };
-
-        //  Create dots and arrows
-        function nav(name, html) {
-            if (name == 'dot') {
-                html = '<ol class="dots">';
-                $.each(_.li, function (index) {
-                    html += '<li class="' + (index == _.i ? name + ' active' : name) + '">' + ++index + '</li>';
-                });
-                html += '</ol>';
-            } else {
-                html = '<div class="';
-                html = html + name + 's">' + html + name + ' prev">' + _.o.prev + '</div>' + html + name + ' next">' + _.o.next + '</div></div>';
-            };
-
-            _.el.addClass('has-' + name + 's').append(html).find('.' + name).click(function () {
-                var me = $(this);
-                me.hasClass('dot') ? _.stop().to(me.index()) : me.hasClass('prev') ? _.prev() : _.next();
+            ul.animate({
+                'left': '-' + index + '00%',
+                'height': height
+            }, o.speed, o.easing, function () {
+                //切换完成后  修改样式  更新索引
+                el.find('.dot').removeClass('active').eq(index).addClass('active');
+                _.i = index;
+                $.isFunction(o.onAfter) && o.onAfter.call(_, el, li.eq(_.i));
             });
         };
     };
 
+    MoUISlider.prototype.play = function () {
+        var _ = this;
+
+        if (_.o.interval) {
+            clearInterval(_.t);
+            _.t = setInterval(function () {
+                _.to(_.i + 1);
+            }, _.o.interval * 1000);
+        }
+
+        return _;
+
+    };
+    MoUISlider.prototype.stop = function () {
+        clearInterval(this.t);
+        return this;
+    };
+
+    MoUISlider.prototype.next = function () {
+        this.to(this.i + 1);
+    };
+
+    MoUISlider.prototype.prev = function () {
+        this.to(this.i - 1);
+    };
+
+    MoUISlider.prototype._nav = function (name) {
+        var _ = this,
+            html;
+
+
+        if (name == 'dot') {
+            html = '<ol class="dots">';
+            $.each(_.li, function (index) {
+                html += '<li class="' + (index === _.i ? name + ' active' : name) + '"></li>';
+            });
+            html += '</ol>';
+        } else {
+            html = '<div class="arrows">';
+            html += '<div class="arrow prev">' + _.o.prev + '</div>';
+            html += '<div class="arrow next">' + _.o.next + '</div>';
+            html += '</div>';
+        };
+
+        _.ele.addClass('has-' + name + 's').append(html).find('.' + name).click(function () {
+            var me = $(this);
+            me.hasClass('dot') ? _.to(me.index()) : me.hasClass('prev') ? _.prev() : _.next();
+        });
+    };
 
 })(jQuery);
